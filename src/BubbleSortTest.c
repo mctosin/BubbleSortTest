91c37951329f7363bc81dcbaa5e38267dba7a85b
mctosin <mctosin@gmail.com>
2021-01-27 19:55:07 -0300

    teste

--- src/BubbleSortTest.c
+++ src/BubbleSortTest.c
@@ -5,7 +5,7 @@ void bubblesort(int* a, int N) {
 	for (j = N - 1; j > 0; j--) {
 		for (i = 0; i < j; i++) {
 			if (a[i] > a[i + 1]) {
-				aux = a[i + 1];
+				aux = a[i + 1]; //
 				a[i + 1] = a[i];
 				a[i] = aux;
 			}
